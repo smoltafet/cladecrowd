44cd134e23a4363997f6ba84666a2166cd761f72
skneehal <neehal@zinzu.io>
2024-08-11 15:14:27 -0700

    checkout

---
+++ my-app/src/pages/Details/components/Checkout.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import { Modal, Box, Typography, Button, TextField } from '@mui/material';
+import PropTypes from 'prop-types';
+
+function CheckoutModal({ open, handleClose }) {
+  return (
+    <Modal
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="checkout-modal-title"
+      aria-describedby="checkout-modal-description"
+    >
+      <Box sx={{ ...modalStyle }}>
+        <Typography id="checkout-modal-title" variant="h6" component="h2">
+          Checkout
+        </Typography>
+        <Typography id="checkout-modal-description" sx={{ mt: 2 }}>
+          {/* Add your checkout form here */}
+          <TextField
+            label="Name"
+            variant="outlined"
+            fullWidth
+            margin="normal"
+          />
+          <TextField
+            label="Card Number"
+            variant="outlined"
+            fullWidth
+            margin="normal"
+          />
+          <TextField
+            label="Expiration Date"
+            variant="outlined"
+            fullWidth
+            margin="normal"
+          />
+          <TextField
+            label="CVV"
+            variant="outlined"
+            fullWidth
+            margin="normal"
+          />
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() => {
+              // Handle payment logic here
+              handleClose();
+            }}
+          >
+            Pay Now
+          </Button>
+        </Typography>
+      </Box>
+    </Modal>
+  );
+}
+
+CheckoutModal.propTypes = {
+  open: PropTypes.bool.isRequired,
+  handleClose: PropTypes.func.isRequired,
+};
+
+const modalStyle = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
+export default CheckoutModal;
