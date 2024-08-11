619522686f43ac823507939e671bfabdaf9dde2e
amoltafet <amoltafet@expediagroup.com>
2024-08-11 11:13:51 -0700

    header

--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -1,28 +1,23 @@
-import React from 'react';
-import { Button, IconButton } from '@mui/joy';
 import { Link } from 'react-router-dom';
-import { Typography } from "@mui/joy";
-import cladeLogo from '../images/logo2.png'; // Adjust the path as necessary
-
-import theme from '../theme';
+import { Button, IconButton, TextField } from '@mui/material';
+import cladeLogo from '../images/logo2.png'; // Update the path to your logo
 
 const Header = () => {
   const styles = {
     navbar: {
       display: 'flex',
-      justifyContent: 'space-between',
       alignItems: 'center',
-      backgroundColor: theme.colors.primary,
-      padding: '10px 20px',
-      flexWrap: 'wrap', // Allow wrapping for smaller screens
+      justifyContent: 'space-between',
+      padding: '10px 20px', // Added vertical padding
+      margin: '0 20px', // Added horizontal margin
+      backgroundColor: '#333',
+      color: 'white',
     },
     navbarBrand: {
-      color: 'white',
-      textDecoration: 'none',
-      fontSize: '1.5em',
-      fontWeight: 'bold',
       display: 'flex',
       alignItems: 'center',
+      textDecoration: 'none',
+      color: 'white',
     },
     navbarLinks: {
       display: 'flex',
@@ -35,6 +30,10 @@ const Header = () => {
     },
     menuButton: {
       display: 'none',
+    },
+    searchBar: {
+      flexGrow: 1,
+      margin: '0 20px', // Center the search bar by adding horizontal margin
     },
     '@media (max-width: 600px)': {
       navbarLinks: {
@@ -52,10 +51,17 @@ const Header = () => {
         <img src={cladeLogo} alt="Logo" style={styles.logo} />
         Clade
       </Link>
+      <TextField
+        variant="outlined"
+        size="small"
+        placeholder="Search..."
+        style={styles.searchBar}
+      />
       <div style={styles.navbarLinks}>
         <Link to="/" style={{ color: 'white', textDecoration: 'none' }}>Landing</Link>
         <Link to="/details" style={{ color: 'white', textDecoration: 'none' }}>Details</Link>
-        <Button variant="outlined" color="primary" size='medium'>Sign Up</Button>
+        <Button variant="outlined" color="primary" size='medium'>Login</Button>
+        <Button variant="contained" color="primary" size='medium'>Sign Up</Button>
       </div>
       <IconButton style={styles.menuButton} color="inherit">
         <span className="material-icons">menu</span>
